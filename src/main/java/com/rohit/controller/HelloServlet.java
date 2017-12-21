8e17faf6477baf9255ff97649783976da065f9b9
rohit.patro <rohit.patro.consultant@nielsen.com>
2017-12-21 16:04:22 +0530

    initial commit

---
+++ src/main/java/com/rohit/controller/HelloServlet.java
@@ -0,0 +1,26 @@
+package com.rohit.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public HelloServlet() {
+		super();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String yourName = request.getParameter("yourName");
+		PrintWriter writer = response.getWriter();
+		writer.println("<h1>Hello " + yourName + "</h1>");
+		writer.close();
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<h1>Hello JSP and Servlet!</h1>
+<form action="helloServlet" method="post">
+	Enter your name: <input type="text" name="yourName" size="20">
+	<input type="submit" value="Call Servlet" />
+</form>
+
+<a href="login.html">Login</a>
